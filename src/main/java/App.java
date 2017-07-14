7b98fc6e8a24c65a657d062293ef02138dc129ff
Saul Artiga <slbrtg@gmail.com>
2017-07-14 15:14:48 -0700

    Add route for /

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,22 @@
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.Map;
+import java.util.HashMap;
 
 public class App {
 
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (req, res) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new VelocityTemplateEngine().render(
+        new ModelAndView(model, layout)
+      );
+    });
+
   }
 }
